6e3759ae24903159b0fa100917cf56f6d264742e
Karen Javadyan <kajarenc@gmail.com>
2024-05-30 21:02:07 +0400

    add login with Github flow

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
+import requests
 import streamlit as st
 
-st.title("Hello World!")
+st.title("Auth demo!")
 
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+
+@st.cache_data
+def get_github_user_identity(access_token):
+    if not access_token:
+        return None
+
+    headers = {"Authorization": "token " + access_token}
+    response = requests.get(
+        "https://api.github.com/user",
+        headers=headers,
+    )
+
+    data = response.json()
+
+    if not data.get("email"):
+        response = requests.get(
+            "https://api.github.com/user/emails",
+            headers=headers,
+        )
+        data["email"] = next(
+            email["email"] for email in response.json() if email["primary"]
+        )
+    return data
+
 
 google_button = st.button("Login WITH Google")
 
@@ -21,7 +44,17 @@ microsoft_login = st.button("Microsoft Login")
 if microsoft_login:
     st.experimental_user.login(send_redirect_to_host=False, provider="microsoft")
 
+github_login = st.button("Github Login")
+
+if github_login:
+    st.experimental_user.login(send_redirect_to_host=False, provider="github")
+
 st.write(st.experimental_user)
+
+if st.experimental_user.get("provider") == "github":
+    github_access_token = st.experimental_user.get("access_token")
+    st.write(":sparkles: :rainbow[Github user data]")
+    st.write(get_github_user_identity(github_access_token))
 
 
 logout_button = st.button("Logout")
