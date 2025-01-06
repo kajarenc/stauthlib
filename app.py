a3874e50e32ccd9ed649b30001aa41c51d85d6e5
Karen Javadyan <kajarenc@gmail.com>
2025-01-06 17:44:35 +0400

    update latest wheel file

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import streamlit as st
 st.title("Auth demo!")
 
 
+slider_value = st.slider("Pick a number", 0, 100)
+st.write("You picked:", slider_value)
+
 with st.echo():
-    st.write("Is user logged in?", st.experimental_user.is_authenticated())
+    st.write("Is user logged in?", st.experimental_user.get("is_logged_in"))
 
 left, middle, right, logout_button_column = st.columns(4)
 
@@ -13,18 +16,18 @@ with left:
     google_button = st.button("Login with Google")
 
     if google_button:
-        st.experimental_user.login(provider="google")
+        st.login(provider="google")
 
 with middle:
     auth_zero_login = st.button("Auth0 Login")
     if auth_zero_login:
-        st.experimental_user.login(provider="auth0")
+        st.login(provider="auth0")
 
 with right:
     microsoft_login = st.button("Microsoft Login")
 
     if microsoft_login:
-        st.experimental_user.login(provider="microsoft")
+        st.login(provider="microsoft")
 
 
 st.write(":sparkles: :rainbow[User data]")
@@ -34,4 +37,4 @@ st.write(st.experimental_user)
 with logout_button_column:
     logout_button = st.button("Logout")
     if logout_button:
-        st.experimental_user.logout()
+        st.logout()
